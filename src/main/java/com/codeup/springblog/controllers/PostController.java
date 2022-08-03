345044b1add4a5e23720615ea18bcac4c8bbee35
orgeron.kevin <orgeron.kevin@gmail.com>
2022-08-03 09:42:14 -0500

    finished exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,36 +1,51 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    public String postsPage(){
-//        return "<h1>These are all of the posts on this page</h1>";
+    public String postsPage(Model model){
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post(1L, "Post 1", "This is my post 1"));
+        posts.add(new Post(2L, "Post 2", "This is my post 2"));
+        posts.add(new Post(3L, "Post 3", "This is my post 3"));
+        model.addAttribute("posts", posts);
         return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    public String postByIdPage(@PathVariable long id){
-//        return String.format("This is the post for %d", id);
-        return "/posts/view";
+    public String postByIdPage(@PathVariable long id, Model model){
+        List<Post> posts = new ArrayList<>();
+        posts.add(new Post(1L, "Post 1", "This is my post 1"));
+        posts.add(new Post(2L, "Post 2", "This is my post 2"));
+        posts.add(new Post(3L, "Post 3", "This is my post 3"));
+
+        model.addAttribute("post", posts.get((int)id - 1));
+
+        return "/posts/show";
     }
 
-    @GetMapping("/posts/create")
-    public String createPostPage(){
-//        return "<h1>Create post here!</h1>";
-        return "/posts/create";
-    }
 
-    @PostMapping("/posts/create")
-    public String createdPost(){
-//        return "created post";
-        return null;
-    }
+
+//    @GetMapping("/posts/create")
+//    public String createPostPage(){
+//        return "/posts/create";
+//    }
+//
+//    @PostMapping("/posts/create")
+//    public String createdPost(){
+//        return null;
+//    }
 }
