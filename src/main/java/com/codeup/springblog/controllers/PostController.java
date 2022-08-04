e975a82483ddbd3cc0ab525752ba250b13161854
orgeron.kevin <orgeron.kevin@gmail.com>
2022-08-04 10:29:15 -0500

    added the edit functionality

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,41 +29,57 @@ public class PostController {
     }
 
     @PostMapping("/posts")
-    public String deletePost(Long delete){
+    public String deletePost(Long delete, Long edit, HttpSession session){
+
+        if (edit != null) {
+            session.setAttribute("edit", edit);
+            return "/posts/edit";
+        }
 
         if (delete != null) {
             Post post = pr.getById(delete);
             pr.delete(post);
         }
-
-
         return "redirect:/posts";
     }
 
-    @GetMapping("/posts/{id}")
-    public String postByIdPage(@PathVariable long id, Model model){
-        List<Post> posts = new ArrayList<>();
-        posts.add(new Post(1L, "Post 1", "This is my post 1"));
-        posts.add(new Post(2L, "Post 2", "This is my post 2"));
-        posts.add(new Post(3L, "Post 3", "This is my post 3"));
+//    @GetMapping("/posts/{id}")
+//    public String postByIdPage(@PathVariable long id, Model model){
+//        List<Post> posts = new ArrayList<>();
+//        posts.add(new Post(1L, "Post 1", "This is my post 1"));
+//        posts.add(new Post(2L, "Post 2", "This is my post 2"));
+//        posts.add(new Post(3L, "Post 3", "This is my post 3"));
+//
+//        model.addAttribute("post", posts.get((int)id - 1));
+//
+//        return "/posts/show";
+//    }
 
-        model.addAttribute("post", posts.get((int)id - 1));
-
-        return "/posts/show";
-    }
-
-
-
+//THESE HANDLE THE CREATION OF POSTS
     @GetMapping("/post/create")
-    public String createPostPage( String title, String body){
+    public String createPostPage(){
 
         return "/posts/create";
     }
 
     @PostMapping("/posts/create")
-    public String createdPost( String title, String body){
+    public String createPost( String title, String body){
         if (title != null && body != null) {
             pr.save(new Post(title, body));
+        }
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/post/edit")
+    public String editPostPage(){
+        return "/posts/edit";
+    }
+
+    @PostMapping("/posts/edit")
+    public String editPost( String title, String body, HttpSession session){
+        Long id = (Long) session.getAttribute("edit");
+        if (title != null && body != null) {
+            pr.editPost(title, body, id);
         }
         return "redirect:/posts";
     }
--- src/main/java/com/codeup/springblog/repos/PostRepository.java
+++ src/main/java/com/codeup/springblog/repos/PostRepository.java
@@ -2,7 +2,18 @@ package com.codeup.springblog.repos;
 
 import com.codeup.springblog.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Post p SET p.title = :title, p.body = :body WHERE p.id = :id")
+    Integer editPost(@Param ("title") String title, @Param("body") String body, @Param("id") Long id);
 
 }
